bef68522257a7f8f95eba7e2b5145245c425ecf5
Mikhail Eremkin <mikhail.eremkin@umbrellait.com>
2022-10-26 12:43:39 +0300

    Fix kotlin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,25 @@
 plugins {
+    kotlin("jvm")
     id("java-library")
     id("maven-publish")
+}
+
+repositories {
+    mavenCentral()
 }
 
 group = "com.umbrellait.android.vanilla"
 version = "1.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
 
 publishing {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,4 +5,16 @@ pluginManagement {
     }
 }
 
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    }
+}
+
 rootProject.name = "module_inject"
